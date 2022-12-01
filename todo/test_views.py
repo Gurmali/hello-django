437ec223bc26b1aca97005c5cd4bb52b97886a18
Gurmali <Gurmali@hotmail.com>
2022-12-01 15:33:23 +0000

    Testing models and coverage

--- todo/test_views.py
+++ todo/test_views.py
@@ -5,7 +5,7 @@ from .models import Item
 # Create your tests here.
 
 
-class testViews(TestCase):
+class TestViews(TestCase):
 
     def test_get_todo_list(self):
         response = self.client.get('/')
@@ -40,3 +40,10 @@ class testViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+    
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
